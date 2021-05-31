686910c477bcf4ec52d466a8a88bef79053b7e23
Антон Войцишевский <grygry0@gmail.com>
2021-05-31 23:33:32 +0500

    WIP: add auth to tapir;

--- src/main/scala/io/funfunfine/lacrude/announcement/api/LacrudeRoutes.scala
+++ src/main/scala/io/funfunfine/lacrude/announcement/api/LacrudeRoutes.scala
@@ -1,101 +1,102 @@
 package io.funfunfine.lacrude.announcement.api
 
+import cats.Monad
 import cats.MonadThrow
 import cats.implicits._
-
 import tofu.WithRun
 import tofu.generate.GenUUID
 import tofu.syntax.context._
 import tofu.syntax.feither._
 import tofu.syntax.foption._
-
 import io.funfunfine.lacrude.announcement.Announcement
 import io.funfunfine.lacrude.announcement.AnnouncementService
-
 import cats.effect.Concurrent
 import cats.effect.ContextShift
 import cats.effect.Timer
-
+import io.funfunfine.lacrude.users.User
+import io.funfunfine.lacrude.users.User
+import io.funfunfine.lacrude.users.UsersService
 import sttp.tapir._
 import sttp.tapir.json.circe._
 import sttp.tapir.server.http4s.Http4sServerInterpreter
-
 import org.http4s.HttpRoutes
+import sttp.tapir.model.UsernamePassword
+import sttp.tapir.server.PartialServerEndpoint
+import tofu.Delay
 
-object AnnouncementEndpoints {
+import java.util.UUID
+import scala.util.Try
+
+class AnnouncementEndpoints[F[_]: Timer: ContextShift: GenUUID: Concurrent, G[_]: WithRun[*[_], F, RequestContext]: MonadThrow](
+                                                                                                                                 announcementService: AnnouncementService[G] ,usersService: UsersService[F]) {
+def contextEndpoint[I, O](f: I => G[O]): (User.Id, I) => F[O] = {
+case (id, input) =>
+for {
+trace  <- RequestContext.make[F](id)
+output <- runContext[G](f(input))(trace)
+} yield output
+}
+
+def authedEndpoint[I, IP, O](f: I => F[O]): IP => F[O]
+
+def handleErrors[I, O](f: I => F[O]): I => F[Either[String, O]] =
+input => f(input).attempt.leftMapIn(_.getMessage)
+
+  private def authUser =
+    (up: UsernamePassword) => for {
+    uuid <- Delay[F].delay[Option[UUID]](Try(UUID.fromString(up.username)).toOption)
+    id <- (uuid, up.password).tupled.flatTraverse {
+      case (id, pwd) =>
+        val userId = User.Id(id)
+       usersService.isRegistered( pwd, userId).map(if (_) userId.some else none)}
+  } yield Either.fromOption(id, "Non authorized")
 
   def definitions: List[Endpoint[_, _, _, _]] =
     createAnnouncement :: readAnnouncements :: readAnnouncement :: updateAnnouncement :: deleteAnnouncement :: Nil
-  val baseEndpoint: Endpoint[Unit, String, Unit, Any] = endpoint.in("announcements").errorOut(stringBody)
 
-  val createAnnouncement: Endpoint[Announcement.Data, String, Announcement.Id, Any] =
+  private def baseEndpoint: PartialServerEndpoint[UsernamePassword, User.Id, Unit, String, Unit, Any, F] =
+    endpoint.in("announcements").errorOut(stringBody).in(auth.basic[UsernamePassword]()).serverLogicForCurrent[User.Id, F](authUser)
+
+  val createAnnouncement =
     baseEndpoint.post
       .in(jsonBody[Announcement.Data])
       .out(plainBody[Announcement.Id].description("Айди добавленного объявления"))
       .description("Добавить объявление")
+      .serverLogic(handleErrors(contextEndpoint(announcementService.create(_).map(_.id)))
 
-  val readAnnouncement: Endpoint[Announcement.Id, String, Announcement, Any] = baseEndpoint.post
+  val readAnnouncement = baseEndpoint.post
     .in(path[Announcement.Id]("announcement_id"))
     .out(jsonBody[Announcement])
-    .description("Прочитать объявление по его идентификатору")
-
-  val updateAnnouncement: Endpoint[(Announcement.Id, Announcement.Patch), String, Unit, Any] = baseEndpoint.put
-    .in(path[Announcement.Id]("announcement_id"))
-    .in(jsonBody[Announcement.Patch])
-    .description("Обновить данные в объявлении")
-
-  val readAnnouncements: Endpoint[Unit, String, List[Announcement], Any] = //TODO should be streaming
-    baseEndpoint.get.out(jsonBody[List[Announcement]])
-
-  val deleteAnnouncement: Endpoint[Announcement.Id, String, Unit, Any] = baseEndpoint.delete
-    .in(path[Announcement.Id]("announcement_id").description("Идентификатор удаляемого объявления"))
-    .description("Удалить объявление")
-
-}
-
-object AnnouncementRoutes {
-
-  def routes[F[_]: Timer: ContextShift: GenUUID: Concurrent, G[_]: WithRun[*[_], F, RequestContext]: MonadThrow](
-      announcementService: AnnouncementService[G]
-  ): HttpRoutes[F] = {
-
-    def contextEndpoint[I, O](f: I => G[O]): I => F[O] =
-      input =>
-        for {
-          trace  <- RequestContext.make[F]
-          output <- runContext[G](f(input))(trace)
-        } yield output
-
-    def handleErrors[I, O](f: I => F[O]): I => F[Either[String, O]] =
-      input => f(input).attempt.leftMapIn(_.getMessage)
-
-    Http4sServerInterpreter.toRoutes(AnnouncementEndpoints.createAnnouncement)(
-      handleErrors(contextEndpoint(announcementService.create(_).map(_.id)))
-    ) <+>
-      Http4sServerInterpreter.toRoutes(AnnouncementEndpoints.readAnnouncement)(
-        handleErrors(
-          contextEndpoint[Announcement.Id, Announcement](
-            id =>
-              announcementService.getAll
-                .map(_.find(_.id == id))
-                .orThrow(new Throwable("No such announcement"))
-          )
-        )
-      ) <+>
-      Http4sServerInterpreter.toRoutes(AnnouncementEndpoints.readAnnouncements)(
-        handleErrors(
-          contextEndpoint(
-            _ => announcementService.getAll
-          )
-        )
-      ) <+>
-      Http4sServerInterpreter.toRoutes(AnnouncementEndpoints.updateAnnouncement)(
-        handleErrors(contextEndpoint {
-          case (id, patch) => announcementService.update(id, patch)
-        })
-      ) <+>
-      Http4sServerInterpreter.toRoutes(AnnouncementEndpoints.deleteAnnouncement)(
-        handleErrors(contextEndpoint(announcementService.delete))
+    .description("Прочитать объявление по его идентификатору").serverLogic(   authedEndpoint[Announcement.Id, ]{
+    handleErrors(
+      contextEndpoint[Announcement.Id, Announcement](
+        id =>
+          announcementService.getAll
+            .map(_.find(_.id == id))
+            .orThrow(new Throwable("No such announcement"))
       )
-  }
+    )
+  })
+
+  val updateAnnouncement =
+    baseEndpoint.put
+      .in(path[Announcement.Id]("announcement_id"))
+      .in(jsonBody[Announcement.Patch])
+      .description("Обновить данные в объявлении").serverLogic(handleErrors(contextEndpoint {
+      case (id, patch) => announcementService.update(id, patch)
+    }))
+
+  val readAnnouncements = //TODO should be streaming
+    baseEndpoint.get.out(jsonBody[List[Announcement]]).serverLogic( handleErrors(
+      contextEndpoint(
+        _ => announcementService.getAll
+      )
+    ))
+
+  val deleteAnnouncement = baseEndpoint.delete
+    .in(path[Announcement.Id]("announcement_id").description("Идентификатор удаляемого объявления"))
+    .description("Удалить объявление").serverLogic( handleErrors(contextEndpoint(announcementService.delete)))
+
+  def routes: HttpRoutes[F] = ???
 }
+
--- src/main/scala/io/funfunfine/lacrude/announcement/api/RequestContext.scala
+++ src/main/scala/io/funfunfine/lacrude/announcement/api/RequestContext.scala
@@ -1,16 +1,17 @@
 package io.funfunfine.lacrude.announcement.api
 
 import derevo.derive
-
 import cats.Functor
-
+import io.funfunfine.lacrude.users.User
 import tofu.generate.GenUUID
 import tofu.logging.derivation.loggable
 import tofu.syntax.monadic._
 
 @derive(loggable)
-final case class RequestContext(traceId: String)
+final case class RequestContext(traceId: String, userId: User.Id)
 
 object RequestContext {
-  def make[F[_]: GenUUID: Functor]: F[RequestContext] = GenUUID.randomString[F].map(RequestContext.apply _)
+
+  def make[F[_]: GenUUID: Functor](userId: User.Id): F[RequestContext] =
+    GenUUID.randomString[F].map(RequestContext.apply(_, userId))
 }
